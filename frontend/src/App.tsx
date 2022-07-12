2b252aee34077c25eeddf1d86d56806a65e64deb
POLLY1515 <amarantepolly2030@gmail.com>
2022-07-12 20:21:04 -0300

    Frist component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
-
+import NotificationButton from "./componentes/NotificationButton"
 
 function App() {
   return (
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+
+    </>
   )
   
   
---
+++ frontend/src/componentes/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import icon  from "../../recursos/IMG/notification.svg"
+import './style.css'
+function NotificationButton() {
+  return (
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="notificar"/>
+
+    </div>
+  )
+
+
+}
+
+export default NotificationButton
